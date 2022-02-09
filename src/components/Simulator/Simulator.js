2441b71ef7b775abe5c695e3e741daf62f2192fb
mssdesign <matheussilvasoares43@gmail.com>
2022-02-09 20:54:02 -0300

    calc

--- src/components/Simulator/Simulator.js
+++ src/components/Simulator/Simulator.js
@@ -1,16 +1,35 @@
 import './Simulator.css'
+import info from '../../assets/info.svg'
 
 const Simulator = (props) => {
-    function simulateInvestments() {
-        props.onSimulate()
-    }
+  function simulateInvestments() {
+    props.onSimulate()
+  }
 
-    return (
-        <div>
-            simulator
-            <button onClick={simulateInvestments}>click-me</button>
+  return (
+    <div className="simulator">
+      <h3 className="simulator-title">Simulador</h3>
+
+      <div className="income section">
+        <div className="income-top">
+          <h5>Rendimento</h5>
+          <img src={info} alt="mais informaões"></img>
         </div>
-    )
+      </div>
+
+      <div className="indexing section">
+        <div className="indexing-top">
+          <h5>Tipos de indexação</h5>
+          <img src={info} alt="mais informaões"></img>
+        </div>
+      </div>
+
+      <div className="actions">
+        <button onClick={simulateInvestments}>Limpar campos</button>
+        <button onClick={simulateInvestments}>Simular</button>
+      </div>
+    </div>
+  )
 }
 
 export default Simulator

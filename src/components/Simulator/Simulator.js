1d8a7719ff21ca3ae3233595fa811f77e27fae6e
mssdesign <matheussilvasoares43@gmail.com>
2022-02-09 21:30:14 -0300

    calc

--- src/components/Simulator/Simulator.js
+++ src/components/Simulator/Simulator.js
@@ -10,23 +10,32 @@ const Simulator = (props) => {
     <div className="simulator">
       <h3 className="simulator-title">Simulador</h3>
 
-      <div className="income section">
-        <div className="income-top">
-          <h5>Rendimento</h5>
-          <img src={info} alt="mais informaões"></img>
+      <div className="section">
+        <div className='income'>
+            <div className="income-top">
+              <h5>Rendimento</h5>
+              <img src={info} alt="mais informaões"></img>
+            </div>
+
+            <div className='income-buttons'>
+                <button>Bruto</button>
+                <button>Líquido</button>
+            </div>
         </div>
       </div>
 
-      <div className="indexing section">
-        <div className="indexing-top">
-          <h5>Tipos de indexação</h5>
-          <img src={info} alt="mais informaões"></img>
+      <div className="section">
+        <div className='indexing'>
+            <div className="indexing-top">
+              <h5>Tipos de indexação</h5>
+              <img src={info} alt="mais informaões"></img>
+            </div>
         </div>
       </div>
 
       <div className="actions">
         <button onClick={simulateInvestments}>Limpar campos</button>
-        <button onClick={simulateInvestments}>Simular</button>
+        <button onClick={simulateInvestments} id="simulate-action">Simular</button>
       </div>
     </div>
   )

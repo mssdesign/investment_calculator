c38c5725083018a2773dc93301d688a48e8039b8
mssdesign <matheussilvasoares43@gmail.com>
2022-02-12 23:15:24 -0300

    CALC

--- src/components/Graphic/Graphic.js
+++ src/components/Graphic/Graphic.js
@@ -1,20 +1,31 @@
 import './Graphic.css'
 import api from '../../services/api'
+import React, { useState, useEffect } from 'react'
 
 const Graphic = (props) => {
     //http://localhost:3000/simulacoes?tipoIndexacao=pos&tipoRendimento=bruto
     //props.searchData[0]
+    
+    let incomeSearch = props.searchData[0]
+    let indexingSearch = props.searchData[1]
+    const [dataFetched, setDataFetched] = useState(null)
 
     async function getSimulationValues() {
         await api
-          .get('/simulacoes')
+          .get(`/simulacoes?tipoIndexacao=${indexingSearch}&tipoRendimento=${incomeSearch}`)
           .then((response) => {
-            
+            console.log(response.data)
+            setDataFetched(response.data)
           })
           .catch((err) => {
             console.log('Ocorreu um erro!' + err)
           })
     }
+
+    useEffect(() => {
+        getSimulationValues()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <div className='results'>
@@ -23,27 +34,27 @@ const Graphic = (props) => {
             <div className='board'>
                 <div className='card'>
                     <h5>Valor final bruto</h5>
-                    <p>{props.searchData[0]}</p>                    
+                    <p>{!dataFetched ? 'Carregando...' : dataFetched[0]['valorFinalBruto']}</p>                    
                 </div>
                 <div className='card'>
                     <h5>Aliquota do IR</h5>
-                    <p>R$ 15.509,27</p>
+                    <p>{!dataFetched ? 'Carregando...' : dataFetched[0]['aliquotaIR']}</p>
                 </div>
                 <div className='card'>
                     <h5>Valor pago em IR</h5>
-                    <p>R$ 15.509,27</p>
+                    <p>{!dataFetched ? 'Carregando...' : dataFetched[0]['valorPagoIR']}</p>
                 </div>
                 <div className='card'>
                     <h5>Valor final líquido</h5>
-                    <p>R$ 15.509,27</p>
+                    <p>{!dataFetched ? 'Carregando...' : dataFetched[0]['valorFinalLiquido']}</p>
                 </div>
                 <div className='card'>
                     <h5>Valor total investido</h5>
-                    <p>R$ 15.509,27</p>
+                    <p>{!dataFetched ? 'Carregando...' : dataFetched[0]['valorTotalInvestido']}</p>
                 </div>
                 <div className='card'>
                     <h5>Ganho líquido</h5>
-                    <p>R$ 15.509,27</p>
+                    <p>{!dataFetched ? 'Carregando...' : dataFetched[0]['ganhoLiquido']}</p>
                 </div>
             </div>
 

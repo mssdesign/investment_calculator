25567716fdf7ee103691476463c56ebfce8ea5f9
mssdesign <matheussilvasoares43@gmail.com>
2022-02-13 15:39:54 -0300

    calc

--- src/components/Graphic/Chart.js
+++ src/components/Graphic/Chart.js
@@ -13,7 +13,7 @@ const Chart = (props) => {
         `/simulacoes?tipoIndexacao=${indexingSearch}&tipoRendimento=${incomeSearch}`
       )
       .then((response) => {
-        console.log(response.data[0]['graficoValores'])
+        console.log(response.data[0]['graficoValores']['comAporte'][0])
         setChartDataFetched(response.data[0]['graficoValores'])
       })
       .catch((err) => {
@@ -31,17 +31,77 @@ const Chart = (props) => {
       <h4 className="chart-title">Projeção de Valores</h4>
       <div className="chart">
         <ul className="bars">
-            <li><div className='bar'></div><div className='bar2'></div><span>0</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>1</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>2</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>3</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>4</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>5</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>6</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>7</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>8</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>9</span></li>
-            <li><div className='bar'></div><div className='bar2'></div><span>10</span></li>
+          <li>
+            <div className='bar'>
+                <div
+                  className="com-aporte"
+                  style={
+                    chartDataFetched
+                      ? { height: `${chartDataFetched['comAporte'][0] / 100}%` }
+                      : { height: '5%' }
+                  }
+                ></div>
+                <div
+                  className="sem-aporte"
+                  style={
+                    chartDataFetched
+                      ? { height: `${chartDataFetched['semAporte'][0] / 100}%` }
+                      : { height: '5%' }
+                  }
+                ></div>
+            </div>
+            <span>0</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>1</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>2</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>3</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>4</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>5</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>6</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>7</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>8</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>9</span>
+          </li>
+          <li>
+            <div className="com-aporte"></div>
+            <div className="sem-aporte"></div>
+            <span>10</span>
+          </li>
         </ul>
       </div>
     </div>

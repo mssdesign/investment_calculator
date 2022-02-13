2b1a85a1be8c45a0d15b102c315c1d5b91ea5cf5
mssdesign <matheussilvasoares43@gmail.com>
2022-02-13 15:43:01 -0300

    calc

--- src/components/Graphic/Chart.js
+++ src/components/Graphic/Chart.js
@@ -32,74 +32,234 @@ const Chart = (props) => {
       <div className="chart">
         <ul className="bars">
           <li>
-            <div className='bar'>
-                <div
-                  className="com-aporte"
-                  style={
-                    chartDataFetched
-                      ? { height: `${chartDataFetched['comAporte'][0] / 100}%` }
-                      : { height: '5%' }
-                  }
-                ></div>
-                <div
-                  className="sem-aporte"
-                  style={
-                    chartDataFetched
-                      ? { height: `${chartDataFetched['semAporte'][0] / 100}%` }
-                      : { height: '5%' }
-                  }
-                ></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][0] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][0] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
             </div>
             <span>0</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][1] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][1] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>1</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][2] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][2] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>2</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][3] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][3] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>3</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][4] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][4] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>4</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][5] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][5] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>5</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][6] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][6] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>6</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][7] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][7] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>7</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][8] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][8] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>8</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][9] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][9] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>9</span>
           </li>
           <li>
-            <div className="com-aporte"></div>
-            <div className="sem-aporte"></div>
+            <div className="bar">
+              <div
+                className="com-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['comAporte'][10] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+              <div
+                className="sem-aporte"
+                style={
+                  chartDataFetched
+                    ? { height: `${chartDataFetched['semAporte'][10] / 100}%` }
+                    : { height: '5%' }
+                }
+              ></div>
+            </div>
             <span>10</span>
           </li>
         </ul>

aca47d97c87b17062310042130a7bfcda816711c
mssdesign <matheussilvasoares43@gmail.com>
2022-02-13 10:11:20 -0300

    calc

--- src/components/Graphic/Chart.js
+++ src/components/Graphic/Chart.js
@@ -3,33 +3,49 @@ import api from '../../services/api'
 import React, { useState, useEffect } from 'react'
 
 const Chart = (props) => {
-    let incomeSearch = props.searchData[0]
-    let indexingSearch = props.searchData[1]
-    const [chartDataFetched, setChartDataFetched] = useState(null)
+  let incomeSearch = props.searchData[0]
+  let indexingSearch = props.searchData[1]
+  const [chartDataFetched, setChartDataFetched] = useState(null)
 
-    async function getChartValues() {
-        await api
-          .get(`/simulacoes?tipoIndexacao=${indexingSearch}&tipoRendimento=${incomeSearch}`)
-          .then((response) => {
-            console.log(response.data[0]['graficoValores'])
-            setChartDataFetched(response.data[0]['graficoValores'])
-          })
-          .catch((err) => {
-            console.log('Ocorreu um erro!' + err)
-          })
-    }
+  async function getChartValues() {
+    await api
+      .get(
+        `/simulacoes?tipoIndexacao=${indexingSearch}&tipoRendimento=${incomeSearch}`
+      )
+      .then((response) => {
+        console.log(response.data[0]['graficoValores'])
+        setChartDataFetched(response.data[0]['graficoValores'])
+      })
+      .catch((err) => {
+        console.log('Ocorreu um erro!' + err)
+      })
+  }
 
-    useEffect(() => {
-        getChartValues()
+  useEffect(() => {
+    getChartValues()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  }, [])
 
-    return (
-        <div>
-            <h4>Projeção de Valores</h4>
-            <p>data</p>
-        </div>
-    )
+  return (
+    <div>
+      <h4>Projeção de Valores</h4>
+      <div className="chart">
+        <ul className="bars">
+            <li><div className='bar'></div><span>0</span></li>
+            <li><div className='bar'></div><span>1</span></li>
+            <li><div className='bar'></div><span>2</span></li>
+            <li><div className='bar'></div><span>3</span></li>
+            <li><div className='bar'></div><span>4</span></li>
+            <li><div className='bar'></div><span>5</span></li>
+            <li><div className='bar'></div><span>6</span></li>
+            <li><div className='bar'></div><span>7</span></li>
+            <li><div className='bar'></div><span>8</span></li>
+            <li><div className='bar'></div><span>9</span></li>
+            <li><div className='bar'></div><span>10</span></li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Chart

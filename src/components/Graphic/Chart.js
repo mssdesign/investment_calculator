c6b21b5386a33f5b706694bf31dba969c78fda61
mssdesign <matheussilvasoares43@gmail.com>
2022-02-13 10:35:53 -0300

    calc

--- src/components/Graphic/Chart.js
+++ src/components/Graphic/Chart.js
@@ -28,20 +28,20 @@ const Chart = (props) => {
 
   return (
     <div>
-      <h4>Projeção de Valores</h4>
+      <h4 className="chart-title">Projeção de Valores</h4>
       <div className="chart">
         <ul className="bars">
-            <li><div className='bar'></div><span>0</span></li>
-            <li><div className='bar'></div><span>1</span></li>
-            <li><div className='bar'></div><span>2</span></li>
-            <li><div className='bar'></div><span>3</span></li>
-            <li><div className='bar'></div><span>4</span></li>
-            <li><div className='bar'></div><span>5</span></li>
-            <li><div className='bar'></div><span>6</span></li>
-            <li><div className='bar'></div><span>7</span></li>
-            <li><div className='bar'></div><span>8</span></li>
-            <li><div className='bar'></div><span>9</span></li>
-            <li><div className='bar'></div><span>10</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>0</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>1</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>2</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>3</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>4</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>5</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>6</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>7</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>8</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>9</span></li>
+            <li><div className='bar'></div><div className='bar2'></div><span>10</span></li>
         </ul>
       </div>
     </div>
